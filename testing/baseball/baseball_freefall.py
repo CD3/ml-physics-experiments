937bb7e8d2a5e8a6da45effd4a3e81a073f8a627
CD Clark III <clifton.clark@gmail.com>
2024-04-23 08:36:01 -0500

    added support for running different models for baseball freefall tests

--- testing/baseball/baseball_freefall.py
+++ testing/baseball/baseball_freefall.py
@@ -42,6 +42,9 @@ tau = (vt / g).to("s").magnitude
 def fall_distance(t):
     return tau * vt.to("m/s").magnitude * numpy.log(numpy.cosh(t / tau))
 
+def fall_distance_no_drag(t):
+    return 0.5*g*t**2
+
 
 @click.command()
 @click.argument("model_files", type=pathlib.Path, nargs=-1)
@@ -50,22 +53,44 @@ def main(model_files):
         print(f"Running {model_file}")
         results_file = model_file.with_suffix(".txt")
         model = keras.saving.load_model(model_file)
+        num_inputs = model.get_layer("dense").input.shape[1]
+        if num_inputs == 4:
+                print("model input: vt (no), dy(no)")
+        if num_inputs == 5:
+                print("model input: vt (yes), dy(no)")
+        if num_inputs == 6:
+                print("model input: vt (yes), dy(yes)")
 
         dt = 0.1
         N = 100
         ts = [dt * i for i in range(N)]
-        inputs = np.array(
-            [
-                [
-                    m.to_base_units().magnitude,
-                    k.to_base_units().magnitude,
-                    g.to_base_units().magnitude,
-                    t,
-                    vt.to_base_units().magnitude,
-                ]
-                for t in ts
-            ]
-        )
+        inputs = []
+        for t in ts:
+            if num_inputs == 4:
+                inputs.append( [
+                            m.to_base_units().magnitude,
+                            k.to_base_units().magnitude,
+                            g.to_base_units().magnitude,
+                            t,
+                        ])
+            if num_inputs == 5:
+                inputs.append( [
+                            m.to_base_units().magnitude,
+                            k.to_base_units().magnitude,
+                            g.to_base_units().magnitude,
+                            t,
+                            vt.to_base_units().magnitude,
+                        ])
+            if num_inputs == 6:
+                inputs.append( [
+                            m.to_base_units().magnitude,
+                            k.to_base_units().magnitude,
+                            g.to_base_units().magnitude,
+                            t,
+                            vt.to_base_units().magnitude,
+                            fall_distance_no_drag(t).to_base_units().magnitude,
+                        ])
+        inputs = np.array(inputs)
 
         ys = [fall_distance(t) for t in ts]
         model_ys = model.predict(inputs, verbose=False)
